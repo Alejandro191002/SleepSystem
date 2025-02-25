e476df68f0b220ef091d4a2265454c7b91f84861
Miku <leocastellanoscasillas@gmail.com>
2025-02-24 23:51:12 -0600

    Cambios de diseño en la pagina principal, se agregó homecontroller.cs en controllers, cambios en layout, index, style y se agregó al wwwroot imágenes

---
+++ SleepSystem/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SleepSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Acción para la página principal
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
